c7415ef8756d46e47f74c5e1c7e6b8fb5f501394
QiushengChu <634948946@qq.com>
2017-10-04 22:59:58 +1100

    Tim test

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
+#include <fstream>
+using namespace std;
 int main()
 {
 	std::cout << "message" << std::endl;
     std::cout << "Ben's change here"<<std::endl;
 	std::cout << "Another change -R" << std::endl;
+	std::cout << "123" << std::endl;
 	return 0;
 }
